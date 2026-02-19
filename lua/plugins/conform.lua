071b53e8a4fd78f0793cac8591069a43fdab01b9
makesomelayouts <makesomelayouts@gmail.com>
2026-02-19 05:49:45 +0400

    Add prettier as a default for web dev

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        css = { "prettier" },
+        scss = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+    },
+  },
+}

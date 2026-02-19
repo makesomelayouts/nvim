f8a753132ffcce1755fe0a05bfc490f0f6ec6bff
makesomelayouts <makesomelayouts@gmail.com>
2026-02-19 04:09:39 +0400

    Ignore weird diagnostics about tailwind in css files

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {
+          settings = {
+            css = {
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            scss = {
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}

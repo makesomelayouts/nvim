c9bdcdd3d7d946f9259f3738ab31ca469bfaabeb
makesomelayouts <makesomelayouts@gmail.com>
2026-06-23 03:59:14 +0400

    fix(treesitter): disable auto install

--- init.lua
+++ init.lua
@@ -4,3 +4,12 @@ require("config.lazy")
 if vim.fn.exists(":GuiFont") ~= 0 then
   vim.cmd([[GuiFont! CaskaydiaCove Nerd Font Mono:h16]])
 end
+
+-- Mute popups because of downloading Treesitter errors
+vim.g.skip_ts_error = true
+
+-- Disaable autodownloading new parsers
+require("nvim-treesitter.configs").setup({
+  auto_install = false,
+  sync_install = false,
+})

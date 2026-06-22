1245c3039cd84c04a5bd3d33f1052b6ebc7b41de
makesomelayouts <makesomelayouts@gmail.com>
2026-06-22 15:39:26 +0400

    feat(lsp): enable emmet autocomplete for jsx and web formats
    Exactly for: css, html, js, react, less, sass, scss, pug, ts

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,24 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        emmet_language_server = {
+          filetypes = {
+            "css",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "pug",
+            "typescriptreact",
+          },
+          init_options = {
+            showExpandedAbbreviation = "always",
+            showAbbreviationSuggestions = true,
+            showSuggestionsAsSnippets = true,
+          },
+        },
         cssls = {
           settings = {
             css = {

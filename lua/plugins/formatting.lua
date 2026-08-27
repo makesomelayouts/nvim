e2cf8f2e7c275433127e0b96187c067d351698a2
makesomelayouts <makesomelayouts@gmail.com>
2026-08-27 21:47:22 +0400

    fix formatting html

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,12 +3,24 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        html = { "prettier" },
         css = { "prettier" },
         scss = { "prettier" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
       },
+
+      format_on_save = function(bufnr)
+        if vim.bo[bufnr].filetype == "html" then
+          return false
+        end
+
+        return {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        }
+      end,
     },
   },
 }
